98fa28652c808124dfff4caaf7764191cd057102
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2022-11-11 02:50:36 +0200

    Test improved

--- src/test/java/ru/netology/PostTest.java
+++ src/test/java/ru/netology/PostTest.java
@@ -12,14 +12,15 @@ public class PostTest {
         // Предусловия
         given()
             .baseUri("https://postman-echo.com")
-            .body("ABC") // отправляемые данные (заголовки и query можно выставлять аналогично)
+            .contentType("text/plain; charset=UTF-8")
+            .body("1, Doll, Вероника") // отправляемые данные (заголовки и query можно выставлять аналогично)
         // Выполняемые действия
         .when()
             .post("/post")
         // Проверки
         .then()
             .statusCode(200)
-            .body("data", equalTo("ABCD"))
+            .body("data", equalTo("1, Doll, Вероника"))
         ;
     }
 }
