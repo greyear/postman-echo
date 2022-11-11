f2f8347ee4ea6719c7ca3602f450516ae0bbbe52
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2022-11-11 02:38:46 +0200

    Returned value in test was changed to crash test

--- src/test/java/ru/netology/PostTest.java
+++ src/test/java/ru/netology/PostTest.java
@@ -19,7 +19,7 @@ public class PostTest {
         // Проверки
         .then()
             .statusCode(200)
-            .body("data", equalTo("ABC"))
+            .body("data", equalTo("ABCD"))
         ;
     }
 }
